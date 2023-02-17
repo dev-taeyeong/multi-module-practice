c73d1fd639d15c7ea399871df4938ee7e91fc44a
dev-taeyeong <dev.taeyeong@gmail.com>
2023-02-18 03:26:51 +0900

    first commit

---
+++ module-api/src/main/java/dev/be/moduleapi/controller/DemoController.java
@@ -0,0 +1,29 @@
+package dev.be.moduleapi.controller;
+
+import dev.be.moduleapi.service.DemoService;
+import dev.be.modulecommon.enums.CodeEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class DemoController {
+
+    private final DemoService demoService;
+
+    @GetMapping("/save")
+    public String save() {
+        return demoService.save();
+    }
+
+    @GetMapping("/find")
+    public String find() {
+        return demoService.find();
+    }
+
+    @GetMapping("/exception")
+    public String exception() {
+        return demoService.exception();
+    }
+}
---
+++ module-api/src/main/java/dev/be/moduleapi/response/CommonResponse.java
@@ -0,0 +1,37 @@
+package dev.be.moduleapi.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import dev.be.modulecommon.enums.CodeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CommonResponse<T> {
+
+    private String returnCode;
+    private String returnMessage;
+    private T info;
+
+    public CommonResponse(CodeEnum codeEnum) {
+        this.returnCode = codeEnum.getCode();
+        this.returnMessage = codeEnum.getMessage();
+    }
+
+    public CommonResponse(T info) {
+        this.returnCode = CodeEnum.SUCCESS.getCode();
+        this.returnMessage = CodeEnum.SUCCESS.getMessage();
+        this.info = info;
+    }
+
+    public CommonResponse(CodeEnum codeEnum, T info) {
+        this.returnCode = codeEnum.getCode();
+        this.returnMessage = codeEnum.getMessage();
+        this.info = info;
+    }
+}
